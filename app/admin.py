3359147088e10ae1d2a15d137f4ae4f83dbff7d5
Guillermo Carrasco Faus <gcarrascofaus@gmail.com>
2024-05-30 12:27:18 -0400

    Register models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Region, Comuna, User, Property, Solicitude
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(User)   
+admin.site.register(Property)   
+admin.site.register(Solicitude)
+
